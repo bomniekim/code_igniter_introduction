5b48d23ce3612cd729a06f855d7ec8973aae1da9
Bomin Kim <bomnie.kim@gmail.com>
2020-01-14 17:34:20 +0900

    view를 로드하면서 controller에서 전달받은 데이터 보여주기: 데이터 연관배열의 key값을 변수명으로 사용

---
+++ application/views/trans/first.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Transmission</title>
+</head>
+<body>
+
+    <!-- Controller클래스로부터 전달받은 데이터($data) 연관배열의 요소 하나하나를 변수로 사용 -->
+    <!-- 연관배열의 'key'값을 변수명으로 사용 가능 -->
+    <h2> 이름: <?=$name?></h2>
+    <p> 메세지: <?=$msg?></p>    
+</body>
+</html>
