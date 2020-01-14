62c0f4acb2db0ecfe799b6be2a9593981b263f2f
Bomin Kim <bomnie.kim@gmail.com>
2020-01-14 17:46:39 +0900

    뷰 문서를 통해 화면에 보여주기(MVC의 종속성)

---
+++ application/views/member/member_view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Member</title>
+</head>
+<body>
+
+    <h2>Member</h2>
+    <h5>name: <?=$name?></h5>
+    <h5>message: <?=$msg?></h5>
+    <!-- MVC의 종속성을 보여주는 예 -->
+    
+</body>
+</html>
