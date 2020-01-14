5391fa054f141b0b44ec49cb11a78bd18abfb1ef
Bomin Kim <bomnie.kim@gmail.com>
2020-01-14 17:31:24 +0900

    view 문서를 따로 제작하여 공통모듈 작업하기

---
+++ application/views/module/content.php
@@ -0,0 +1,13 @@
+    <h2>Pengsoo</h2>
+
+    <p>Pengsoo is so cute</p>
+
+    <!-- 이미지의 경로는 [절대경로] 사용 (http://)-->
+
+    <!-- 상대경로는 무조건 root폴더(code_igniter)가 기준 -->
+    <!-- 단, 상대경로는 index()에서만 load할 때 사용가능 -->
+
+    <img src="http://localhost/code_igniter/images/pengsoo.jpg" alt="pengsoo">
+
+</body>
+</html>
---
+++ application/views/module/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Intro module</title>
+</head>
+<body>
+
+    <header style="text-align: center; background: lightblue; padding-top:20px; padding-bottom:20px;">
+        <h1>This is Header of the module</h1>
+        <p>module test page</p>
+    </header>
+    
