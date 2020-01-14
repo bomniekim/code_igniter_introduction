14f31a09fd349d0a70e48f8e423ddb7b3ee987e8
Bomin Kim <bomnie.kim@gmail.com>
2020-01-14 17:28:58 +0900

    하나의 function 안에서 여러 view 로드 가능

---
+++ application/views/intro_show2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Intro Show View2</title>
+</head>
+<body>
+
+    <h2>Show Second</h2>
+    <p>This is show's second method.</p>
+    
+</body>
+</html>
